fefa2ea0e71f321f867dc4ddebc6ad405d0b11aa
kushagra <kushagragangwal@gmail.com>
2025-06-24 09:43:15 +0530

    Add project files.

---
+++ GD.Practical/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GD.Practical.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
